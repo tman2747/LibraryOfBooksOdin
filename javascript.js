a807c22149c1e2ca2afbf9ee362364e0df4895cd
Triston Land <tristonland@gmail.com>
2024-11-14 01:54:47 -0700

    adjust form and prevent default on submit

--- javascript.js
+++ javascript.js
@@ -1,5 +1,10 @@
 let library = []
-libraryId = 0
+let libraryId = 0
+
+let form = document.getElementById("myForm")
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+})
 
 function Book(title,author,pageCount,readStatus)
 {
