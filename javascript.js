2e5506258607ddd9bfae03b669c18debec70da1b
Triston Land <tristonland@gmail.com>
2024-12-19 15:12:25 -0700

    initial refactor to using classes

--- javascript.js
+++ javascript.js
@@ -1,130 +1,146 @@
-let library = []
-let libraryId = 0
+
 
 let form = document.getElementById("myForm")
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", (e) =>
+{
     e.preventDefault();
     title = form.querySelector("#title").value
     author = form.querySelector("#author").value
     pages = form.querySelector("#pages").value
     readStatus = form.querySelector("#readStatus").checked
-    addBookToLibrary(new Book(title,author,pages,readStatus))
+    addBookToLibrary(new Book(title, author, pages, readStatus))
     renderBooks()
     resetForm()
 })
 
-function Book(title,author,pageCount,readStatus)
+class Book
 {
-    this.title = title
-    this.author = author
-    this.pageCount = pageCount
-    this.readStatus = readStatus
-    this.id = null
+    constructor(title, author, pageCount, readStatus)
+    {
+        this.title = title
+        this.author = author
+        this.pageCount = pageCount
+        this.readStatus = readStatus
+        this.id = null
+    }
 
-    this.print = function()
+    print()
     {
         console.log(`Title: ${this.title}\nAuthor: ${this.author}\nPage Count: ${this.pageCount}\nRead Status: ${this.readStatus} `)
     }
-
-    this.toggleReadStatus = function()
+    toggleReadStatus()
     {
         this.readStatus = !this.readStatus;
     }
+
 }
+class library
+{
+    library = []
+    libraryId = 0
+    constructor()
+    {
+
+    }
+    addBookToLibrary(book)
+    {
+        book.id = this.libraryId;
+        this.libraryId += 1;
+        this.library.push(book)
+    }
+
+    resetForm()
+    {
+        form.querySelector("#title").value = ""
+        form.querySelector("#author").value = ""
+        form.querySelector("#pages").value = ""
+        form.querySelector("#readStatus").checked = false
+    }
+    deleteBook(id)
+    {
+        index = this.library.findIndex(book => book.id === id)
+        if (index == -1)
+        {
+            console.log("You tried deleting a id that doesnt exisit check book indexing. this should not happpen")
+            return
+        }
+        this.library.splice(index, 1)
+        console.log(index)
+        renderBooks()
+    }
+    createBook(bookInfo)
+    {
+
+        let bookcard = document.createElement("div")
+        bookcard.className = "card"
+        bookcard.id = bookInfo.id
+        let topitems = document.createElement("div")
+        topitems.className = "top-items"
+        let title = document.createElement("div")
+        title.innerHTML = `Title: ${bookInfo.title}`
+        title.className = 'card-title'
+        let author = document.createElement("div")
+        author.innerHTML = `Author: ${bookInfo.author}`
+        author.className = 'card-author'
+        let pageCount = document.createElement("div")
+        pageCount.innerHTML = `Page Count: ${bookInfo.pageCount}`
+        pageCount.className = 'card-page-count'
+        let readStatus = document.createElement("div")
+        readStatus.innerHTML = `Read status: ${bookInfo.readStatus}`
+        readStatus.className = "card-read-status"
+        topitems.appendChild(title)
+        topitems.appendChild(author)
+        topitems.appendChild(pageCount)
+        topitems.appendChild(readStatus)
+        bookcard.appendChild(topitems)
+        let buttondiv = document.createElement("div")
+        buttondiv.className = "card-buttons"
+        let removeButton = document.createElement("button")
+        removeButton.innerHTML = "Remove"
+        removeButton.addEventListener("click", () =>
+        {
+            deleteBook(bookInfo.id)
+        })
+
+
+
+
+        let toggleReadStatus = document.createElement("button")
+        toggleReadStatus.innerHTML = "Read / Unread "
+        toggleReadStatus.addEventListener("click", () =>
+        {
+            bookInfo.toggleReadStatus()
+            renderBooks()
+        })
+
+
+        buttondiv.appendChild(toggleReadStatus)
+        buttondiv.appendChild(removeButton)
+        bookcard.appendChild(buttondiv)
+        return bookcard
+    }
+    renderBooks()
+    {
+        let cardContainer = document.getElementsByClassName("card-container")[0]
+        cardContainer.innerHTML = ""
+        this.library.forEach(book =>
+        {
+            cardContainer.appendChild(this.createBook(book))
+        })
+    }
+
+}
+
+
+let mylibrary = new library
 
 let newbook = new Book("1984", "George Orwell", 300, true)
 let newbook1 = new Book("The Hitchhiker’s Guide to the Galaxy", "Douglas Adams", 400, true)
 let newbook2 = new Book("Consider Phlebas", "Iain Banks", 250, true)
 
+console.log(newbook2.title)
+mylibrary.addBookToLibrary(newbook)
+mylibrary.addBookToLibrary(newbook1)
+mylibrary.addBookToLibrary(newbook2)
 
-function addBookToLibrary(book)
-{
-    book.id = libraryId;
-    libraryId += 1;
-    library.push(book)
-}
-
-function resetForm()
-{
-    form.querySelector("#title").value = ""
-    form.querySelector("#author").value = ""
-    form.querySelector("#pages").value = ""
-    form.querySelector("#readStatus").checked = false
-}
-
-addBookToLibrary(newbook)
-addBookToLibrary(newbook1)
-addBookToLibrary(newbook2)
-
-function createBook(bookInfo)
-{
-    
-    let bookcard = document.createElement("div")
-    bookcard.className = "card"
-    bookcard.id = bookInfo.id
-    let topitems = document.createElement("div")
-    topitems.className="top-items"
-    let title = document.createElement("div")
-    title.innerHTML=`Title: ${bookInfo.title}`
-    title.className = 'card-title'
-    let author = document.createElement("div")
-    author.innerHTML=`Author: ${bookInfo.author}`
-    author.className = 'card-author'
-    let pageCount = document.createElement("div")
-    pageCount.innerHTML=`Page Count: ${bookInfo.pageCount}`
-    pageCount.className = 'card-page-count'
-    let readStatus = document.createElement("div")
-    readStatus.innerHTML=`Read status: ${bookInfo.readStatus}`
-    readStatus.className = "card-read-status"
-    topitems.appendChild(title)
-    topitems.appendChild(author)
-    topitems.appendChild(pageCount)
-    topitems.appendChild(readStatus)
-    bookcard.appendChild(topitems)
-    let buttondiv = document.createElement("div")
-    buttondiv.className = "card-buttons"
-    let removeButton = document.createElement("button")
-    removeButton.innerHTML = "Remove"
-    removeButton.addEventListener("click", ()=>{
-        deleteBook(bookInfo.id)
-    })
-
-    
-
-
-    let toggleReadStatus = document.createElement("button")
-    toggleReadStatus.innerHTML = "Read / Unread "
-    toggleReadStatus.addEventListener("click", ()=>{
-        bookInfo.toggleReadStatus()
-        renderBooks()
-    })
-
-    
-    buttondiv.appendChild(toggleReadStatus)    
-    buttondiv.appendChild(removeButton)
-    bookcard.appendChild(buttondiv)
-    return bookcard
-}
-
-function deleteBook(id)
-{
-    index = library.findIndex(book => book.id === id)
-    if (index == -1)
-    {
-        console.log("You tried deleting a id that doesnt exisit check book indexing. this should not happpen")
-        return
-    }
-    library.splice(index,1)
-    console.log(index)
-    renderBooks()
-}
-
-function renderBooks()
-{
-    cardContainer = document.getElementsByClassName("card-container")[0]
-    cardContainer.innerHTML = ""
-    library.forEach(book => {
-        cardContainer.appendChild(createBook(book))
-    })
-}
-renderBooks()
+mylibrary.renderBooks()
